e7daefefdece4e2b2a31dad1f9e48dcef96c7fe7
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-04-05 13:20:24 +0530

    Home jsx file

---
+++ react-recoil-first/src/Components/Pages/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import "./Home.css"
+import SingleProduct from '../../SingleProduct/SingleProduct';
+import Filter from '../../Filtering/Filter';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { InitialProduct, filterdData } from '../../../Recoil/productRecoil';
+import { useLocation, useSearchParams } from 'react-router-dom';
+
+const Home = () => {
+    const [products, setProducts] = useRecoilState(InitialProduct);
+    const filteredData = useRecoilValue(filterdData);
+    const [searchParams] = useSearchParams();
+    const location = useLocation();
+
+    const handelApi = () => {
+        axios.get("http://localhost:3030/products", {
+            params: {
+                brand: searchParams.getAll("brand"),
+            }
+        }).then(res => {
+            setProducts(res.data)
+        });
+    }
+
+
+    const handelDeleteProduct = (id) => {
+        axios.delete(`http://localhost:3030/products/${id}`).then(res => { 
+            handelApi();
+        }).catch(err=>{
+            console.log(err);
+        })
+    }
+
+
+    useEffect(() => {
+        if (location || products.length === 0) {
+            handelApi()
+        }
+    }, [products.length, location])
+
+
+    return (
+        <div className='container'>
+            <div className='filtering-section'>
+                <h2>Filter By</h2>
+                <Filter />
+            </div>
+            <div className='All-Product-show'>
+                {filteredData.length === 0 ? <>{products?.map((product, index) => (
+                    <SingleProduct key={index} {...product} handelDeleteProduct={handelDeleteProduct} />
+                ))}</> : <>{filteredData?.map((product, index) => (
+                    <SingleProduct key={index} {...product} handelDeleteProduct={handelDeleteProduct} />
+                ))}</>
+
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Home
