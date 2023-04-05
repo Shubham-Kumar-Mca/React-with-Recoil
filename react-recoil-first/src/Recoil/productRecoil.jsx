ef86d6276591327f81a66cb001bbdab661273f21
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-04-05 13:24:36 +0530

    Recoil file

---
+++ react-recoil-first/src/Recoil/productRecoil.jsx
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const InitialProduct = atom({
+    key: "initialProduct",
+    default: []
+})
+
+
+export const filterdData = atom({
+    key : "filtering",
+    default : []
+})
