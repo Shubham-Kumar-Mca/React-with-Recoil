639c1bae3bdeddf8e160fdd78402a35ac06388ff
Shubham Kumar Jaiswal <krshubhamjaiswal2000@gmail.com>
2023-04-06 20:03:48 +0530

    Navbar JSX File

--- react-recoil-first/src/Components/Navbar/Navbar.jsx
+++ react-recoil-first/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./Navbar.css"
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { searchFunctionality, searchProduct } from '../../Recoil/productRecoil'
 
 const Navbar = () => {
+  const [textSearchFunctionality, setTextSearchFunctionality] = useRecoilState(searchFunctionality);
+  const searchProductsData = useRecoilValue(searchProduct);
+
+  console.log("searchProductsData is", searchProductsData);
+
+  const handelClicking = (title) =>{
+    setTextSearchFunctionality(title);
+  }
+
   return (
     <div className='Navbar-container'>
       <div className='logoTitle'>
         <Link to="/">Sasta E-commerce</Link>
+      </div>
+      <div className='search-container'>
+        <div className='SearchBox'>
+          <input type="text" placeholder='Search Product By Name' value={textSearchFunctionality} onChange={e => setTextSearchFunctionality(e.target.value)} />
+        </div>
+        <div className='SearchData' style={{marginTop : searchProductsData.length > 0 ? "1.5rem" : null}}>
+          {searchProductsData.length > 0 ? searchProductsData.map((searchProductData, index) => (
+            <div key={index} className='singleSearchProductData' onClick={()=>handelClicking(searchProductData.title)}>
+              <img src={searchProductData.thumbnail} alt="" width="20px" height="20px"/>
+              <p>{searchProductData.title}</p>
+            </div>
+          )) : null}
+        </div>
       </div>
       <div className='addProductBtn'>
         <Link to="/addProduct">Add Products</Link>
